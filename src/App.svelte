ce13e464995c04890f0cb02904a16f1cdfcbbbd5
JumpyJacko <bobjacklee27@gmail.com>
2025-11-13 14:09:13 +1100

    add: theme toggle button animation

--- src/App.svelte
+++ src/App.svelte
@@ -2,10 +2,11 @@
     import ThemeToggle from "./lib/components/ThemeToggle.svelte";
 </script>
 
-<header class="fixed flex flex-row bg-transparent end-0 m-3">
-    <ThemeToggle />
+<ThemeToggle />
+<header class="fixed flex flex-row bg-transparent">
 </header>
 <main class="flex flex-col justify-center items-center">
     <img src="/images/birb-bg-no-bg.svg" alt="A monochrome, pixel-art bird" class="h-1/4 mt-20 w-fit" />
     <h1 class="text-4xl text-center mt-5">hi there</h1>
 </main>
+
--- src/lib/components/ThemeToggle.svelte
+++ src/lib/components/ThemeToggle.svelte
@@ -3,8 +3,15 @@
 
     let isDark = $state(false);
 
-    onMount(() => { // HACK: fixes site starting on wrong state
+    let sunButton: HTMLButtonElement;
+    let moonButton: HTMLButtonElement;
+
+    onMount(() => {
+        // HACK: fixes site starting on wrong state
         isDark = localStorage.theme !== "dark";
+
+        const currentButton = isDark ? moonButton : sunButton;
+        currentButton.classList.add("hidden");
     });
 
     function toggleTheme() {
@@ -17,9 +24,50 @@
                 (!("theme" in localStorage) &&
                     window.matchMedia("(prefers-color-scheme: dark)").matches),
         );
+
+        if (isDark) {
+            moonButton.addEventListener("animationend", listener);
+            moonButton.classList.add("slide-out");
+
+            function listener(event: AnimationEvent) {
+                if (event.type === "animationend") {
+                    moonButton.classList.add("hidden");
+                    moonButton.classList.remove("slide-out");
+                    sunButton.classList.remove("hidden");
+                    sunButton.classList.add("slide-in");
+                }
+
+                moonButton.removeEventListener("animationend", listener);
+            }
+        } else {
+            sunButton.addEventListener("animationend", listener);
+            sunButton.classList.add("slide-out");
+
+            function listener(event: AnimationEvent) {
+                if (event.type === "animationend") {
+                    sunButton.classList.add("hidden");
+                    sunButton.classList.remove("slide-out");
+                    moonButton.classList.remove("hidden");
+                    moonButton.classList.add("slide-in");
+                }
+
+                sunButton.removeEventListener("animationend", listener);
+            }
+        }
     }
 </script>
 
-<button onclick={toggleTheme}>
-    {isDark ? "go to dark" : "go to light"}
+<button
+    bind:this={sunButton}
+    onclick={toggleTheme}
+    class="absolute"
+>
+    <img src="/icons/sun.svg" width="64" alt="Sun"/>
+</button>
+<button
+    bind:this={moonButton}
+    onclick={toggleTheme}
+    class="absolute"
+>
+    <img src="/icons/moon.svg" width="64" alt="Moon" />
 </button>
